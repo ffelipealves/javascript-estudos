9a37da2cff25360036aeb6b3507c98dd38ffdf75
Felipe Alves <103237188+ffelipealves@users.noreply.github.com>
2022-09-09 18:23:10 -0300

    inicio curso

---
+++ TiposDeSaida/index.js
@@ -0,0 +1,3 @@
+document.getElementById("texto").innerHTML="Aqui estou usando o getElementById para escrever no paragrafo 'texto'";
+alert("Output com alert")
+console.log("Console.log eh mais pro")
---
+++ Variaveis/script.js
@@ -0,0 +1,16 @@
+let a,b,c,d //declarando variaveis
+a=23;
+b="Jorge";
+c=true;
+
+document.write(b)
+document.write(a*a)
+
+//existem 3 tipo de maneiras de declarar variaveis
+
+var num1=20;
+alert(a+num1)
+
+var nome = "Felioe"
+
+document.getElementById("var").innerHTML = nome
