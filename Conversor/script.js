09b1e47b35b5774d2d3d8e0d4dc9c16889263f11
ffelipealves <felipe.alves0850@gmail.com>
2022-09-14 19:50:17 -0300

    conversor de comprimento

---
+++ Conversor/script.js
@@ -0,0 +1,9 @@
+function converteValor(){
+    let comprimento1,comprimento2;
+
+    comprimento1 = document.getElementById("comprimentos_1").value;
+    comprimento2 = document.getElementById("comprimentos_2").value;
+
+    window.alert(comprimento1);
+    window.alert(comprimento2);
+}
