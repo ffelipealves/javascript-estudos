6f82bf798a7d45d97beee8b51ba0974f6eda55fc
ffelipealves <felipe.alves0850@gmail.com>
2022-09-17 20:49:15 -0300

    if-else

--- NumeroAleatorio/script.js
+++ NumeroAleatorio/script.js
@@ -1,25 +1,74 @@
-function advinhaNumero(){
-    var sonicNumero=getRandomInt(4000000)
-
-    var chute= document.getElementById("chute").value
-    chute = parseInt(chute)
-
-    var resposta = document.getElementById("resposta")
-
-    if(chute == sonicNumero){
-        resposta.innerHTML="VC ACERTOU Reposta sonic: "+sonicNumero
-    }else{
-        resposta.innerHTML="VC ERROU, Reposta sonic: "+sonicNumero
-    }
-
-    
-
-    
-}
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
-  
-  
-  // expected output: a number from 0 to <1
+}
+////////////////////////////////////////////////////
+var sonicNumero
+var tentativas
+var resposta = document.getElementById("resposta")
+var elementoTentativa = document.getElementById('tentativas')
+var elementoChute = document.getElementById('chute')
+document.getElementById("resetar").style.display='none'
+////////////////////////////////////////////////////
+
+
+function sonicPensaNumero(){
+    sonicNumero=getRandomInt(3)
+}
+
+
+function iniciaJogo(){
+    
+    sonicPensaNumero()
+    tentativas = 5
+    document.getElementById("iniciar").style.display='none'
+    document.getElementById("telaprincipal").style.display='block'
+    elementoTentativa.innerHTML="Tentativas "+tentativas
+
+}
+
+function chutaNumero(){
+
+    
+    
+    var chute= parseInt(document.getElementById("chute").value)
+    if(chute>sonicNumero){
+        resposta.innerHTML="Dica: Tente um numero menor"
+    }else if(chute<sonicNumero){
+        resposta.innerHTML="Dica: Tente um numero maior"
+    }else{
+        resposta.innerHTML=""
+    }
+
+    if(chute == sonicNumero){
+        fimDeJogo(tentativas)
+    }else{
+        tentativas-=1
+    }
+    if(tentativas==0){
+        fimDeJogo(tentativas)
+    }
+    elementoChute.value=''
+    elementoTentativa.innerHTML="Tentativas "+tentativas
+}
+
+function fimDeJogo(x){
+
+    if(x==0){
+        resposta.style.color="red"
+        resposta.innerHTML="VC PERDEU, Reposta sonic: "+sonicNumero
+        document.getElementById("chute").style.display='none'
+        document.getElementById("botao_chute").style.display='none'
+        elementoTentativa.style.color="black"
+        elementoTentativa.innerHTML="Quer jogar novamente ?"
+        document.getElementById("resetar").style.display='block'
+    }else{
+        resposta.style.color="green"
+        resposta.innerHTML="VC ACERTOU Reposta sonic: "+sonicNumero
+        document.getElementById("chute").style.display='none'
+        document.getElementById("botao_chute").style.display='none'
+        elementoTentativa.style.color="black"
+        elementoTentativa.innerHTML="Quer jogar novamente ?"
+        document.getElementById("resetar").style.display='block'
+    }
+}
+
